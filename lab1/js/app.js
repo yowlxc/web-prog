d7e33aa1767ee583e875faec0b46ff453052831e
Platon Potemkin <platonpotemkin51@gmail.com>
2026-09-08 10:15:00 +0300

    Прописал чуть больше js

--- lab1/js/app.js
+++ lab1/js/app.js
@@ -1,11 +1,28 @@
 const listStudents = document.getElementById('list-students');
 const formStudents = document.getElementById('form-students');
+const dosieStudents = document.getElementById('dosie-students');
 const btnAdd = document.getElementById('btn-add');
+const btnCancel = document.getElementById('btn-cancel');
+const btnBack = document.getElementById('btn-back');
+
+function showSection(section) {
+    // скрывает все секции кроме переданной
+    listStudents.classList.add('hidden');
+    formStudents.classList.add('hidden');
+    dosieStudents.classList.add('hidden');
+
+    section.classList.remove('hidden');
+};
+
 
 btnAdd.addEventListener('click', function() {
-    // прячем список студентов
-    listStudents.classList.add('hidden');
+    showSection(formStudents);
+});
 
-    // показываем форму, убрав класс
-    formStudents.classList.remove('hidden');
+btnCancel.addEventListener('click', function() {
+    showSection(listStudents);
+});
+
+btnBack.addEventListener('click', function() {
+    showSection(listStudents);
 });
